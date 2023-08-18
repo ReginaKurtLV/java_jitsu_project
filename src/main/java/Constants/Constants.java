f38dc40cd1675cc60b2f3e0cf1ad358a8fbd9c9f
ReginaKurtLV <rgaynul@gmail.com>
2023-08-18 12:34:50 -0700

    new config class

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -3,4 +3,6 @@ package Constants;
 public class Constants {
 
     public static final String PROMOTION_PAGE_TEXT = "Revolutionize Your Human Resource Management Experience!";
+    public static final String URL = "https://www.orangehrm.com/";
+
 }

b8e8004ae6946deb0ae037e3e023ab1a81e7b3d7
ReginaKurtLV <rgaynul@gmail.com>
2023-08-18 19:50:52 -0700

    change classes

--- src/main/java/Constants/Constants.java
+++
@@ -1,6 +0,0 @@
-package Constants;
-
-public class Constants {
-
-    public static final String PROMOTION_PAGE_TEXT = "Revolutionize Your Human Resource Management Experience!";
-}
--- src/test/java/test/HomePageTest.java
+++ src/test/java/test/HomePageTest.java
@@ -4,16 +4,25 @@ import Page.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static Constants.Constants.PROMOTION_PAGE_TEXT;
-
 
 public class HomePageTest extends BaseTest {
 
+    public static final String PROMOTION_PAGE_TEXT = "Revolutionize Your Human Resource Management Experience!";
+    public static final String FREE_DEMO_PAGE_TEXT = "Manage Your People and Operations in One Place";
+
     @Test
-    public void FreeTestButton(){
+    public void freeTestButtonTest(){
         HomePage testFreeButton = new HomePage(driver);
 
         testFreeButton.freeButtonSubmit();
         Assert.assertEquals(PROMOTION_PAGE_TEXT, testFreeButton.getTextFree());
+    }
+
+    @Test
+    public void bookFreedomButtonTest(){
+        HomePage testFreedomButton = new HomePage(driver);
+
+        testFreedomButton.bookFreeDemoButtonClick();
+        Assert.assertEquals(FREE_DEMO_PAGE_TEXT, testFreedomButton.getTextFreeDemoPage());
     }
 }
